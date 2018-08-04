4dfea94334208d1132bb5780c6be2720794a5c37
Jino Jossy <jinojossy93@gmail.com>
2018-08-04 16:27:59 +0530

    added new view file uploader and git ignore file

--- file_uploader/file_uploader/urls.py
+++ file_uploader/file_uploader/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from handler import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^serve/([a-zA-Z0-9-_/]+)$', views.ServeFiles, name="files serving"),
 ]
--- file_uploader/handler/models.py
+++ file_uploader/handler/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class File(models.Model):
+    name = models.CharField(max_length=200)
+    location_path = models.CharField(max_length=200)
+    size = models.IntegerField(default=0)
